fc3d0d84073d101a08b65f52bc94fb7db244f6f7
xiaonanpols21 <xiaonanpolspols@gmail.com>
2025-02-18 09:27:34 +0100

    tags update

--- src/components/tags/skills/index.js
+++ src/components/tags/skills/index.js
@@ -3,9 +3,9 @@ import styles from "./tags.module.scss";
 export default function Tags({classes}) {
     return (
         <ul className={`${styles.ul} ${classes}`}>
-            <li>Oog voor detail</li>
             <li>Figma</li>
-            <li>Toegankelijkheid</li>
+            <li>HTML, CSS, JS Skills</li>
+            <li>Oog voor detail</li>
         </ul>
     )
 }
